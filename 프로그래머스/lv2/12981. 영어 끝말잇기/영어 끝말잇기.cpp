ee9cbb915e5bbfa31c87b793ff045d87078a85a6
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2023-01-20 18:56:41 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -1,26 +1,18 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <unordered_set>
 
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
-    unordered_set<string> s;
-    s.insert(words[0]);
+    unordered_set<string> s = {words[0]};
     int cnt = words.size();
     for(int i = 1; i < cnt; i++) {
-        string w = words[i];
         // 끝말잇기 체크, 중복 체크
-        if(words[i - 1].back() != w[0] || s.count(w)) {
-            int num = ++i % n;
-            if(num == 0)
-                return {n, i / n};
-            else
-                return {num, i / n + 1};
-        }
+        if(words[i - 1].back() != words[i][0] || s.count(words[i])) 
+            return {i % n + 1, i / n + 1};
         else
-            s.insert(w);
+            s.insert(words[i]);
     }
     return {0, 0};
 }
