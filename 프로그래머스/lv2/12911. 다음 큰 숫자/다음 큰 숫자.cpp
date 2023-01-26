477e0d33e7cd6ff14798d8eaea8b5abb904c3d47
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2023-01-26 13:32:08 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.77 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int count(int n) {
+    int i;
+    for(i = 0; n != 0; i++) {
+        n &= (n - 1);
+    }
+    return i;
+}
+
+int solution(int n) {
+    int next = n;
+    while(count(n) != count(++next)) {}
+    return next;
+}
