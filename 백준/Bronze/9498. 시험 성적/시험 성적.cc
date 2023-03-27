f360b245b422c4265e5da45f573f77b5780009d9
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2023-03-28 00:43:29 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+    
+    if (n >= 90) {
+        std::cout << 'A';
+    }
+    else if (n >= 80) {
+        std::cout << 'B';
+    }
+    else if (n >= 70) {
+        std::cout << 'C';
+    }
+    else if (n >= 60) {
+        std::cout << 'D';
+    }
+    else {
+        std::cout << 'F';
+    }
+}
