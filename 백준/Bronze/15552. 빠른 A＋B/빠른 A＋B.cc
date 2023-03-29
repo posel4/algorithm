8bcd1a512b5a86cffd73889b4c44aa392448ac03
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2023-03-29 13:21:37 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int n;
+    std::cin >> n;
+    for(int i = 0; i < n; i++) {
+        int a, b;
+        std::cin >> a >> b;
+        std::cout << a + b << "\n";
+    }
+}
