419df41068993934f9a5ec235c5da317b8b0c54d
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2023-03-29 13:05:35 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    for(int i = 1; i < 10; i++) {
+        std::cout << n << " * " << i << " = " << n * i << std::endl;
+    }
+}
