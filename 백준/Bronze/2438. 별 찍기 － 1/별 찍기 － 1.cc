ed0f347bf7e31d24cc8a46e091e586e39b215517
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2023-03-29 13:29:12 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    
+    int n;
+    std::cin >> n;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < i; j++) {
+            std::cout << "*";
+        }
+        std::cout << "\n";
+    }
+}
