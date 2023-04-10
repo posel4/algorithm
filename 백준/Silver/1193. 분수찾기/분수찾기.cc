a09adfd32e58e1444c7aebbf5daf50f724fee908
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2023-04-10 23:29:07 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.cc
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -1,22 +1,15 @@
 #include <iostream>
 
 int main() {
-    int x, c = 1, s = 1;
+    int x, c = 0, s = 0;
     std::cin >> x;
-    if(x == 1) {
-        std::cout << "1/1";
-        return 0;
+    s = x;
+    while(s > 0) {
+        s -= c++;
     }
-
-    while(++c) {
-        if(s + c + 1 > x)
-            break;
-        else
-            s += c;
-    }
-    int d = x - 1 - s;
+    s += c - 1;
     if(c % 2 == 0)
-        std::cout << 1 + d << "/" << c - d;
+        std::cout << c - s << "/" << s;
     else
-        std::cout << c - d << "/" << 1 + d;
+        std::cout << s << "/" << c - s;
 }
