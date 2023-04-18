b0ecf953049ac6407694a0e6a03d9ebd591ae408
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2023-04-19 00:04:38 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -1,27 +1,12 @@
 #include <iostream>
-using namespace std;
- 
-int main(int argc, char *argv[])
-{
-	int x, y;
- 
-	while (1) {
-		cin >> x >> y;
- 
-		if (x == 0 && y == 0) {
-			break;
-		}
- 
-		if (y % x == 0) {
-			cout << "factor" << endl;
-		}
-		else if (x % y == 0) {
-			cout << "multiple" << endl;
-		}
-		else {
-			cout << "neither" << endl;
-		}
-	}
- 
-	return 0;
+
+int main() {
+    int x, y;
+    while (true) {
+        std::cin >> x >> y;
+        if(x == 0 && y == 0) return 0;
+        if(y % x == 0) std::cout << "factor\n";
+        else if(x % y == 0) std::cout << "multiple\n";
+        else std::cout << "neither\n";
+    }
 }
