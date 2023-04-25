1e75c237a94faf611a3831d3244fffb381a84797
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2023-04-25 20:50:35 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+    while (1)
+    {
+        int a, b, c;
+        std::cin >> a >>  b >> c;
+        if (a == 0 && b == 0 && c == 0)
+        {
+            break;
+        }
+        if (a == b && b == c && c == a)
+        {
+            printf("Equilateral\n");
+        }
+        else if (a >= b + c || b >= c + a || c >= a + b)
+        {
+            printf("Invalid\n");
+        }
+        else if (a == b || b == c || c == a)
+        {
+            printf("Isosceles\n");
+        }
+        else if (a != b && b != c && c != a)
+        {
+            printf("Scalene\n");
+        }
+    }
+    return 0;
+}
