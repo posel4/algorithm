3f828792f30c57ef91e66f6b8cd3adac55d3833d
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2024-01-13 01:14:55 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 220 ms, Memory: 10896 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<algorithm>
+
+std::vector<int> adj[100001];
+bool visited[100001] = {false, };
+int result[100001];
+int cnt = 0;
+
+void dfs(int here) {
+    visited[here] = true;
+    result[here] = ++cnt;
+
+    for (int i = 0; i < adj[here].size(); i++) {
+        int there = adj[here][i];
+        if(!visited[there])
+            dfs(there);
+    }
+}
+
+int main() {
+    std::cin.tie(0);
+    std::cout.tie(0);
+    
+    int n, m, r;
+    std::cin >> n >> m >> r;
+    while(m--) {
+        int u, v;
+        std::cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    for (int i = 1; i <= n; i++) {
+        std::sort(adj[i].begin(), adj[i].end());
+    }
+    dfs(r);
+    for (int i = 1; i <= n; i++) {
+        std::cout << result[i] << "\n";
+    }
+}
