44e29d7e56699d80e9e0fe2a2eeaeba62d43482a
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2024-01-13 21:57:06 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 204 ms, Memory: 8804 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+#define endl "\n"
+
+std::vector<int> adj[100001];
+bool discovered[100001] = {false };
+std::queue<int> q;
+int result[100001] = { 0 };
+int cnt = 0;
+
+void bfs(int start) {
+    discovered[start] = true;
+    q.push(start);
+
+    while(!q.empty()) {
+        int here = q.front();
+        q.pop();
+        result[here] = ++cnt;
+        for(int i = 0; i < adj[here].size(); ++i) {
+            int there = adj[here][i];
+            if(!discovered[there]) {
+                q.push(there);
+                discovered[there] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    int n, m, r;
+    int u, v;
+    std::cin >> n >> m >> r;
+
+    while(m--) {
+        std::cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        std::sort(adj[i].begin(), adj[i].end(), std::greater<int>());
+    }
+
+    bfs(r);
+
+    for(int i = 1; i <= n; ++i) {
+        std::cout << result[i] << endl;
+    }
+
+    return 0;
+}
