43fa19d766d499d1ad0b2441e6b66efe18165208
포슬 <106955460+posel4@users.noreply.github.com>
2024-10-05 18:04:51 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 200 ms, Memory: 9436 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -2,56 +2,61 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define endl "\n"
 
-std::vector<int> adj[100001];
-bool discovered[100001] = {false };
-std::queue<int> q;
-int result[100001] = { 0 };
-int cnt = 0;
+using namespace std;
+
+vector<vector<int>> adj;
+vector<int> visited;
+int n, m, r, cnt = 0;
+
+void input() {
+    cin >> n >> m >> r;
+    adj.resize(n + 1);
+    visited.resize(n + 1);
+
+    while(m--) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    for(int i = 1; i <= n; i++) {
+        sort(adj[i].begin(), adj[i].end());
+    }
+}
 
 void bfs(int start) {
-    discovered[start] = true;
+    queue<int> q;
+
     q.push(start);
+    visited[start] = true;
 
     while(!q.empty()) {
         int here = q.front();
         q.pop();
-        result[here] = ++cnt;
-        for(int i = 0; i < adj[here].size(); ++i) {
-            int there = adj[here][i];
-            if(!discovered[there]) {
+
+        visited[here] = ++cnt;
+
+        for(int there : adj[here]) {
+            if(!visited[there]) {
                 q.push(there);
-                discovered[there] = true;
+                visited[there] = true;
             }
         }
     }
 }
 
+void print() {
+    for(int i = 1; i <= n; i++) {
+        cout << visited[i] << endl;
+    }
+}
+
 int main() {
-    std::cin.tie(0);
-    std::cout.tie(0);
-
-    int n, m, r;
-    int u, v;
-    std::cin >> n >> m >> r;
-
-    while(m--) {
-        std::cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
-    for(int i = 1; i <= n; ++i) {
-        std::sort(adj[i].begin(), adj[i].end(), std::less<int>());
-    }
-
+    input();
     bfs(r);
-
-    for(int i = 1; i <= n; ++i) {
-        std::cout << result[i] << endl;
-    }
-
-    return 0;
+    print();
 }
