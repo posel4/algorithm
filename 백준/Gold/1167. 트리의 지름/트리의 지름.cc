de028615a44f1e5f648e8d8a22ff22e712a45c07
포슬 <106955460+posel4@users.noreply.github.com>
2024-10-06 21:47:28 +0900

    [Gold II] Title: 트리의 지름, Time: 88 ms, Memory: 12216 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -1,50 +1,63 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 #include <cstring>
-#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-#define MAX 100001
-using namespace std;
 
-vector <pair<int, int>> graph[MAX];
-int visited[MAX] = { 0, };
-int n, maxdist = 0, maxnode;
+using namespace std;
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl "\n"
+#define MAX 100001
+
+int n, maxDist = 0, farthest = 0;
+bool visited[MAX];
+vector<pair<int, int>> graph[MAX];
 
 void input() {
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	for(int i = 1; i <= n; i++) {
 		int u, v, w;
 		cin >> u >> v;
 		while (v != -1) {
 			cin >> w;
 			graph[u].push_back({ v, w });
-			graph[v].push_back({ u, w });
+            graph[v].push_back({ u, w });
 			cin >> v;
 		}
 	}
 }
 
+void clear() {
+	maxDist = 0;
+	memset(visited, false, sizeof(visited));
+}
+
 void dfs(int here, int dist) {
-	if (visited[here])
-		return;
-	if (maxdist < dist) {
-		maxdist = dist;
-		maxnode = here;
-	}
 	visited[here] = true;
+
+	if (dist > maxDist) {
+		maxDist = dist;
+		farthest = here;
+	}
+
 	for (auto& next : graph[here]) {
 		int there = next.first;
 		int weight = next.second;
-		dfs(there, dist + weight);
+		if (!visited[there]) {
+			dfs(there, dist + weight);
+		}
 	}
+}
+
+void print() {
+	cout << maxDist;
 }
 
 int main() {
 	fastio;
 	input();
+	clear();
 	dfs(1, 0);
-	memset(visited, false, sizeof(visited));
-	maxdist = 0;
-	dfs(maxnode, 0);
-	cout << maxdist;
-	return 0;
+	clear();
+	dfs(farthest, 0);
+	print();
 }
