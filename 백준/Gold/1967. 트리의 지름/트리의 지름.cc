710aba14aba310926fa4e5feb5c69d7831e5645a
포슬 <106955460+posel4@users.noreply.github.com>
2024-10-06 21:58:47 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 2872 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MAX 10001
+using namespace std;
+
+vector <pair<int, int>> graph[MAX];
+int visited[MAX] = { 0, };
+int n, maxdist = 0, maxnode;
+
+void input() {
+	cin >> n;
+	for (int i = 0; i < n - 1; i++) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		graph[u].push_back({ v, w });
+		graph[v].push_back({ u, w });
+	}
+}
+
+void dfs(int here, int dist) {
+	if (visited[here])
+		return;
+	if (maxdist < dist) {
+		maxdist = dist;
+		maxnode = here;
+	}
+	visited[here] = true;
+	for (auto& next : graph[here]) {
+		int there = next.first;
+		int weight = next.second;
+		dfs(there, dist + weight);
+	}
+}
+
+int main() {
+	fastio;
+	input();
+	dfs(1, 0);
+	memset(visited, false, sizeof(visited));
+	maxdist = 0;
+	dfs(maxnode, 0);
+	cout << maxdist;
+	return 0;
+}
