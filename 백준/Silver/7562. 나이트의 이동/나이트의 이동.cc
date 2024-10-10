1a820d04d3454ba4c04fed42a78483f13fa35011
포슬 <106955460+posel4@users.noreply.github.com>
2024-10-11 02:27:28 +0900

    [Silver I] Title: 나이트의 이동, Time: 16 ms, Memory: 2508 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -1,71 +1,69 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <climits>
 #include <queue>
-
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define endl "\n"
 #define MAX 301
 
-int discovered[MAX][MAX];
-int dist[MAX][MAX];
-int size;
-// 1사분면에서 움직인다.
-int dx[] = {1,2,2,1,-1,-2,-2,-1};
-int dy[] = {2,1,-1,-2,-2,-1,1,2};
+using namespace std;
 
-void visit(std::queue<std::pair<int, int>>& q, int here_x, int here_y, int there_x, int there_y) {
-        dist[there_x][there_y] = dist[here_x][here_y] + 1;
-        discovered[there_x][there_y] = true;
-        q.push(std::make_pair(there_x, there_y));
+int t, n;
+int hx, hy, wx, wy;
+int visited[MAX][MAX];
+
+int dx[8] = { 1, 2,  2,  1, -1, -2, -2, -1 };
+int dy[8] = { 2, 1, -1, -2, -2, -1,  1,  2 };
+
+struct pos {
+	int x;
+	int y;
+	int cnt;
+};
+
+void bfs(pos start) {
+	queue<pos> q;
+	q.push(start);
+	visited[start.x][start.y] = 1;
+
+	while (!q.empty()) {
+		pos here = q.front();
+		int x = here.x;
+		int y = here.y;
+		int cnt = here.cnt;
+		q.pop();
+
+		if (x == wx && y == wy) {
+			cout << cnt << endl;
+			break;
+		}
+
+		for (int i = 0; i < 8; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+			if (!visited[nx][ny]) {
+				visited[nx][ny] = 1;
+				q.push({ nx, ny, cnt + 1 });
+			}
+		}
+	}
 }
 
-void bfs(int start_x, int start_y, int end_x, int end_y) {
-    discovered[start_x][start_y] = true;
-    std::queue<std::pair<int, int>> q;
-    q.push(std::make_pair(start_x, start_y));
-
-    dist[start_x][start_y] = 0;
-
-    while(!q.empty()) {
-        std::pair<int, int> here = q.front();
-        q.pop();
-
-        int here_x = here.first;
-        int here_y = here.second;
-
-        if(here_x == end_x && here_y == end_y) {
-            break;
-        }
-
-        discovered[here_x][here_y] = true;
-
-        for(int i = 0; i < 8; ++i) {
-            int there_x = here_x + dx[i];
-            int there_y = here_y + dy[i];
-            if(0 <= there_x && there_x < size  && 0 <= there_y && there_y < size && !discovered[there_x][there_y]) {
-                visit(q, here_x, here_y, there_x, there_y);
-            }
-        }
-    }
+void clear() {
+	fill(&visited[0][0], &visited[0][0] + MAX * MAX, 0);
 }
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
+	fastio;
+	cin >> t;
 
-    int t;
-    std::cin >> t;
+	while (t--) {
+		clear();
+		cin >> n >> hx >> hy >> wx >> wy;
+		bfs({ hx, hy, 0 });
+	}
 
-    while(t--) {
-        std::fill(&discovered[0][0], &discovered[MAX - 1][MAX - 1], false);
-        std::fill(&dist[0][0], &dist[MAX - 1][MAX - 1], MAX);
-
-        std::cin >> size;
-        int start_x, start_y, end_x, end_y;
-        std::cin >> start_x >> start_y >> end_x >> end_y;
-
-        bfs(start_x, start_y, end_x, end_y);
-        std::cout << dist[end_x][end_y] << endl;
-    }
-
-    return 0;
+	return 0;
 }
