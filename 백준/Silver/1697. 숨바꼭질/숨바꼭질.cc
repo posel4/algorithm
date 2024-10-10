b864f4d0ce4d1cc7441e49197e10bdf110501f99
포슬 <106955460+posel4@users.noreply.github.com>
2024-10-11 01:26:48 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2676 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,53 +1,60 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <climits>
 #include <queue>
-
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define endl "\n"
 #define MAX 100001
 
-int discovered[MAX];
-int dist[MAX];
+using namespace std;
 
-void visit(std::queue<int>& q, int here, int there) {
-    if(0 <= there && there < MAX && !discovered[there]) {
-        dist[there] = dist[here] + 1 < dist[there] ? dist[here] + 1 : dist[there];
-        discovered[there] = true;
-        q.push(there);
-    }
-}
+int n, k, ans = INT_MAX;
+int visited[MAX];
 
-void bfs(int start) {
-    discovered[start] = true;
-    std::queue<int> q;
-    q.push(start);
+struct pos {
+	int x;
+	int cnt;
+};
 
-    dist[start] = 0;
+void bfs(pos start) {
+	queue<pos> q;
+	q.push(start);
 
-    while(!q.empty()) {
-        int here = q.front();
-        q.pop();
+	while (!q.empty()) {
+		pos here = q.front();
+		int x = here.x;
+		int cnt = here.cnt;
+		q.pop();
 
-        discovered[here] = true;
+		if (x == k) {
+			ans = cnt < ans ? cnt : ans;
+			continue;
+		}
 
-        visit(q, here, here + 1);
-        visit(q, here, here - 1);
-        visit(q, here, here * 2);
-    }
+		int nx = x + 1;
+		if (0 <= nx && nx < MAX && !visited[nx]) {
+			visited[x + 1] = true;
+			q.push({ x + 1, cnt + 1 });
+		}
+		nx = x - 1;
+		if (0 <= nx && nx < MAX && !visited[nx]) {
+			visited[x - 1] = true;
+			q.push({ x - 1, cnt + 1 });
+		}
+		nx = x * 2;
+		if (0 <= nx && nx < MAX && !visited[nx]) {
+			visited[x * 2] = true;
+			q.push({ x * 2, cnt + 1 });
+		}
+
+	}
 }
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
-
-    int n, k;
-    std::cin >> n >> k;
-
-    std::fill(discovered, discovered + MAX, false);
-    std::fill(dist, dist + MAX, MAX);
-
-    bfs(n);
-
-    std::cout << dist[k];
-
-    return 0;
+	fastio;
+	cin >> n >> k;
+	bfs({ n, 0 });
+	cout << ans;
+	return 0;
 }
