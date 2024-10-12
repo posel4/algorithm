2f99ad38eebcec56619a19591246cfe528e3f9df
포슬 <106955460+posel4@users.noreply.github.com>
2024-10-12 20:44:54 +0900

    [Gold II] Title: 친구 네트워크, Time: 204 ms, Memory: 38828 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.cc
@@ -0,0 +1,72 @@
+// union find
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <unordered_map>
+
+typedef long long ll;
+
+using namespace std;
+unordered_map<string, string> um;
+unordered_map<string, int> nums;
+
+
+string find(string a){
+	if(um[a] == a){
+		return a;
+	}
+	else{
+		return um[a] = find(um[a]);
+	}
+}
+
+void uni(string a, string b){
+	string A = find(a);
+	string B = find(b);
+
+	if(A==B) return;
+
+	um[B] = A;
+	nums[A] += nums[B];
+}
+
+int main(int argc, char** argv)
+{	
+
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int tn;
+	cin >> tn;
+
+	while(tn--){
+		int n;
+		cin >> n;
+
+		um.clear();
+		nums.clear();
+		
+		for(int i=0; i<n; i++){
+			string a, b;
+			cin >> a >> b;
+			if(um.count(a) == 0){
+				um.insert(make_pair(a, a));
+				nums.insert(make_pair(a,1));
+			}
+			if(um.count(b) == 0){
+				um.insert(make_pair(b, b));
+				nums.insert(make_pair(b,1));
+			}
+			uni(a,b);
+			cout << nums[find(a)] << '\n';
+
+		}
+	}
+	
+	
+	
+}
